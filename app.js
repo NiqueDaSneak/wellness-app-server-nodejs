1fcd18c3f635bfe2e3d97f638a098ed939fb6784
niquedasneak <niquedasneak4@gmail.com>
2016-05-23 11:16:10 -0400

    add functionality to echo responses back. added function for sending messages

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 var express = require('express');
-var app = express();
 var bodyParser = require('body-parser');
+var request = require('request');
+
+var app = express();
 
 app.use(bodyParser.json());
+
+var verify_token = 'NBRMXSs3sO';
+var token = 'EAADYeYqESZAEBABNZBtUkkgGmrFdFMwJXx7rDhKYblHLRRualIXrEZAbMvoTpw6lxgFWxdH5kFJ1cf409xyoulL1CbLQt63ezTSofslMjo46cyOM2HbCalGfYqgmKVn9TJY6zLl5x61Yf9rTiul7kOdfyvAX3s9vWQBFYGVHQZDZD';
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function(req, res) {
@@ -11,7 +16,7 @@ app.get('/', function(req, res) {
 });
 
 app.get('/webhook/', function (req, res) {
-  if (req.query['hub.verify_token'] === 'NBRMXSs3sO') {
+  if (req.query['hub.verify_token'] === verify_token) {
     res.send(req.query['hub.challenge']);
   }
   res.send('Error, wrong validation token');
@@ -19,16 +24,50 @@ app.get('/webhook/', function (req, res) {
 
 app.post('/webhook/', function (req, res) {
   messaging_events = req.body.entry[0].messaging;
+
   for (i = 0; i < messaging_events.length; i++) {
-    event = req.body.entry[0].messaging[i];
-    sender = event.sender.id;
+    var event = req.body.entry[0].messaging[i];
+    var sender = event.sender.id;
+    
     if (event.message && event.message.text) {
-      text = event.message.text;
+      var text = event.message.text;
       // Handle a text message from this sender
       console.log(text);
+      sendTextMessage(sender, "Echo: " + text.substring(0, 200));
+    
     }
+
   }
   res.sendStatus(200);
 });
 
 app.listen(process.env.PORT || 3000);
+
+
+
+
+function sendTextMessage(sender, text) {
+
+    var messageData = {
+        text: text
+    };
+
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token: token},
+        method: 'POST',
+        json: {
+            recipient: {id: sender},
+            message: messageData
+        }
+    }, function (error, response) {
+
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+
+    });
+
+}
